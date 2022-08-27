68f355ab45da70644783235bc9960b8fd851ff2d
Ravivarma Katari <49902143+ravi-katari@users.noreply.github.com>
2022-08-27 13:21:22 +0530

    Changes to MainActivity | Handling Back press & updated mainFragment call

--- app/src/main/java/com/example/nasa_apod_app/MainActivity.kt
+++ app/src/main/java/com/example/nasa_apod_app/MainActivity.kt
@@ -1,24 +1,33 @@
 package com.example.nasa_apod_app
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.example.nasa_apod_app.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
 
         if (savedInstanceState == null) {
-            loadFragment(MainFragment.newInstance())
+            loadMainFragment()
         }
     }
 
-    fun loadFragment(fragment: Fragment) {
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        supportActionBar?.setDisplayHomeAsUpEnabled(false)
+        return true
+    }
+
+    fun loadMainFragment() {
         supportFragmentManager.beginTransaction()
-            .replace(R.id.container, fragment)
+            .replace(R.id.container, MainFragment.newInstance())
+            .setCustomAnimations(R.anim.slide_in, R.anim.fade_out)
             .commitNow()
     }
 }
+
